73e1fbff9324bc35275bb6f8a50f26794cf5d700
YukiSato <yukiyukiamam@gmail.com>
2025-08-07 19:15:54 +0900

    initial commit

---
+++ serverside/src/main/kotlin/com/personal/keiba/KeibaDashboardApplication.kt
@@ -0,0 +1,11 @@
+package com.personal.keiba
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KeibaDashboardApplication
+
+fun main(args: Array<String>) {
+	runApplication<KeibaDashboardApplication>(*args)
+}
